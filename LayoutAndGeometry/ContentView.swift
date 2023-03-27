05b8b256396935d5bda883d3ff045652ea8b6cf0
derya <derya@boonandmoil.dev>
2023-03-27 10:52:08 +0100

    demonstration of coordinate spaces

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -7,24 +7,44 @@
 
 import SwiftUI
 
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { geo in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            GeometryReader { geo in
-                Text("Hello, world!")
-                    .frame(width: geo.size.width * 0.9)
-                    .background(.red)
-            }
-            .background(.green)
-            
-            Text("More text")
-            Text("More text")
-            Text("More text")
+            OuterView()
+                .background(.red)
+                .coordinateSpace(name: "Custom")
         }
-        
-        
-            
+       
     }
 }
 
