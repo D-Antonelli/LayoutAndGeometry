80bc8d2c535eee9078aa59a3a96ab8e93f89884c
derya <derya@boonandmoil.dev>
2023-03-27 09:13:15 +0100

    demonstration of absolute positioning

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -7,33 +7,12 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            d[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("sample")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello, world!")
+            .position(x: 100, y: 100)
+            .background(.red)
     }
 }
 
